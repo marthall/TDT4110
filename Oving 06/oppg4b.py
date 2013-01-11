c6926caabc7489fbcd5c2ea404fd4822b97f5833
marthall <marthall@outlook.com>
2013-01-11 16:01:57 +0100

    Rewrote exercise

--- Oving 06/oppg4b.py
+++ Oving 06/oppg4b.py
@@ -1,12 +1,21 @@
-teeth = [95, 103, 71, 99, 114, 64, 95, 53, 97, 114, 109, 11, 2, 21, 45, 2, 26, 81, 54, 14, 118, 108, 117, 27, 115, 43, 70, 58, 107]
-number_coins, temp_num_coin, coins, temp_teeth = [0, 0, 0, 0, 0,], [0, 0, 0, 0, 0,], [20, 10, 5,1, 0.5], [(0.5*i) for i in teeth]
+teeth = [95, 103, 71, 99, 114, 64, 95, 53, 97,
+    114, 109, 11, 2, 21, 45, 2, 26, 81, 54, 14,
+    118, 108, 117, 27, 115, 43, 70, 58, 107]
+coins = [20, 10, 5,1, 0.5]
+tot_coins = [0 for i in range(5)]
+tooth_coins = [0 for i in range(5)]       # Used to 
+teeth_value = [(0.5*i) for i in teeth]
+
 print("-------------------------------------------")
 print("Gram:  20kr -- 10kr -- 5kr -- 1kr -- 50orer")
-for i in range(len(teeth)):
-    for j in range(len(coins)):
-        temp_num_coin[j] = (temp_teeth[i] // coins[j])
-        number_coins[j] += temp_num_coin[j]
-        temp_teeth[i] -= (temp_num_coin[j]*coins[j])
-    print("%3i\t" % teeth[i] + "%i\t%i\t%i\t%i\t%i" % tuple(temp_num_coin[0:5]))
-print("Tot:\t%i\t%i\t%i\t%i\t%i" % tuple(number_coins[0:5]))
+
+for i in teeth:
+    toot_value = 0.5 * i
+    for coin_index, coin_value in enumerate(coins):
+        tooth_coins[coin_index] = toot_value // coin_value    # Floor division with coin_value
+        tot_coins[coin_index] += tooth_coins[coin_index]      # Adds number of coins for this tooth to the total
+        toot_value -= tooth_coins[coin_index]*coin_value      # The remaining value of the tooth
+    print("%3i\t" % i + "%i\t%i\t%i\t%i\t%i" % tuple(tooth_coins[0:5]))    # Prints out the information for every tooth
+
+print("Tot:\t%i\t%i\t%i\t%i\t%i" % tuple(tot_coins[0:5]))                           # Prints out the total
 print("--------------------------------------------")
